f9dc10279509306dd65aa23be837e8af8afbb873
codact <savalijey@gmail.com>
2019-10-09 20:57:05 +0300

    Create react app boiler plate

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+const App = () => {
+  return (
+    <div className="App mx-auto">
+      <section className="intro text-center">
+        <i>Oh! Hello. Nice to meet you</i>
+        <h1 className="title">Hi there,
+        My name is James Savali.</h1>
+        <p>
+          I am an experienced software developer witha deep passion in programming.
+        <br></br>
+          I am focused in my passion, and i believe i will positively<br></br> impact the world with my skills.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
