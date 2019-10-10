4df0542a09083512e5e9a73553e76abd4fcd7817
codact <savalijey@gmail.com>
2019-10-10 19:20:07 +0300

    Add padding for mobile view on skills and conatcts

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,22 +7,22 @@ export class Contact extends Component {
             <section class="col-12">
                 <div class="mx-auto">
                     <div class="container">
-                        <header class="mb-5 text-center">
+                        <header class="text-center">
                             <h2 class="text-uppercase">Contact Me</h2>
                             <hr className="normal-hr"></hr>
                         </header> 
                         <div className="container-fluid">
                         <div className="row">
-                            <div className="text-muted col-md-4 text-left">
-                            <i class="fas fa-phone fa-sm"></i>&nbsp;Mobile No: +254708197333<br></br>
-                            <i class="far fa-envelope fa-sm"></i>&nbsp;Email: savalijey@gmail.com
+                            <div className="text-muted col-md-4 text-left mt-5">
+                                <i class="fas fa-phone fa-sm"></i>&nbsp;Mobile No: +254708197333<br></br>
+                                <i class="far fa-envelope fa-sm"></i>&nbsp;Email: savalijey@gmail.com
                             </div>
-                            <div className="col-md-4 text-center">
-                            <a href="https://github.com/dev-jey"  target="_blank" rel="noopener noreferrer" className="text-dark"><i class="fab fa-github fa-2x p-1"></i></a>
+                            <div className="col-md-4 text-md-center mt-5">
+                                <a href="https://github.com/dev-jey"  target="_blank" rel="noopener noreferrer" className="text-dark"><i class="fab fa-github fa-2x p-1"></i></a>
 
-                            <a href="https://www.linkedin.com/in/devjey/"  target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin fa-2x p-1"></i></a>
+                                <a href="https://www.linkedin.com/in/devjey/"  target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin fa-2x p-1"></i></a>
                             </div>
-                            <div className="text-left text-muted col-md-4">
+                            <div className="text-left text-muted col-md-4 mt-5">
                                 <i>"The harder you work for something, the greater you'll feel when you achieve it."<br></br>
                                 -- Anonymous</i>
                             </div>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -10,9 +10,9 @@ export class Skills extends Component {
                             <h2 class="text-uppercase">My Skills</h2>
                             <hr className="normal-hr"></hr>
                         </header>
-                        <div class="row text-center mx-auto mb-5">
+                        <div class="row text-center mx-auto">
                             <div class="col-lg-3 col-md-6 ">
-                                <div class="icon mb-3"><i class="fab fa-python fa-5x"></i></div>
+                                <div class="icon mb-3 mt-5"><i class="fab fa-python fa-5x"></i></div>
                                 <h4 class="text-uppercase">Python</h4>
                                 <p class="text-mted">I am proficient in python web frameworks, data analysis and general algorithms.</p>
                                 Django
@@ -37,7 +37,7 @@ export class Skills extends Component {
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-6 ">
-                                <div class="icon mb-3"><i class="fab fa-js fa-5x"></i></div>
+                                <div class="icon mb-3 mt-5"><i class="fab fa-js fa-5x"></i></div>
                                 <h4 class="text-uppercase">Javascript</h4>
                                 <p class="text-mted">Am a PEAN full stack js developer. I have experience in both front and backend development.</p>
                                 Node-Express
@@ -62,7 +62,7 @@ export class Skills extends Component {
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-6 ">
-                                <div class="icon mb-3"><i class="fab fa-java fa-5x"></i></div>
+                                <div class="icon mb-3 mt-5"><i class="fab fa-java fa-5x"></i></div>
                                 <h4 class="text-uppercase">Java</h4>
                                 <p class="text-mted">Am well informed in OOP, and I can comfortably create a stand-alone app using java swing.</p>
                                 Java
@@ -72,7 +72,7 @@ export class Skills extends Component {
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-6 ">
-                                <div class="icon mb-3"><i class="fas fa-database fa-5x"></i></div>
+                                <div class="icon mb-3 mt-5"><i class="fas fa-database fa-5x"></i></div>
                                 <h4 class="text-uppercase">Database</h4>
                                 <p class="text-mted">I have previously worked with both relational and non-relational database management systems.</p>
                                 PostgreSQL
@@ -92,9 +92,10 @@ export class Skills extends Component {
                                 </div>
                             </div>
                         </div>
+                        <br></br><br></br>
                         <div className="row mx-auto text-center">
                             <div class="col-lg-3 col-md-6">
-                                <div class="icon mb-3"><i class="fab fa-aws fa-5x"></i></div>
+                                <div class="icon mb-3 mt-5"><i class="fab fa-aws fa-5x"></i></div>
                                 <h4 class="text-uppercase">DevOps</h4>
                                 <p class="text-mted">I have some substantial knowledge in server management, web hosting and maintenance.</p>
                                 AWS
@@ -109,7 +110,7 @@ export class Skills extends Component {
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-6 ">
-                                <div class="icon mb-3"><i class="fab fa-bootstrap fa-5x"></i></div>
+                                <div class="icon mb-3 mt-5"><i class="fab fa-bootstrap fa-5x"></i></div>
                                 <h4 class="text-uppercase lined lined-compact">UI &amp; UX</h4>
                                 <p class="text-mted">I have a lot of experience in developing user-friendly UIs, using common templating engines.</p>
                                 HTML
@@ -134,7 +135,7 @@ export class Skills extends Component {
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-6 ">
-                                <div class="icon mb-3"><i class="fas fa-users-cog fa-5x"></i></div>
+                                <div class="icon mb-3 mt-5"><i class="fas fa-users-cog fa-5x"></i></div>
                                 <h4 class="text-uppercase lined lined-compact">Collaboration</h4>
                                 <p class="text-mted">Using collaboration tools suc as github, am able to work
                                  in a team amicably and develop a product under the agile methodology.</p>
@@ -158,7 +159,7 @@ export class Skills extends Component {
                                 </div>
                             </div>
                             <div class="col-lg-3 col-md-6 ">
-                                <div class="icon mb-3"><i class="fas fa-cube fa-5x"></i></div>
+                                <div class="icon mb-3 mt-5"><i class="fas fa-cube fa-5x"></i></div>
                                 <h4 class="text-uppercase lined lined-compact">Testing, Linting & Debugging</h4>
                                 <p class="text-mted">I write clean, linted and well tested code. I ensure
                                  I have a good maintenability rate, and a high test coverage.</p>
