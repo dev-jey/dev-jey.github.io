c12706b34abdfaae1c5ecb3989a7052d0aa5142f
dev-jey <savalijey@gmail.com>
2023-01-04 15:13:38 +0300

    Updates

---
+++ precache-manifest.82f63980a58c74dec8d75ca65cc65b0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2125afe4b2ddd95027b00810441ab4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae8468e925f6e53a0a81",
+    "url": "/static/css/main.db7eb91f.chunk.css"
+  },
+  {
+    "revision": "938db3e6bf6d2070eb20",
+    "url": "/static/js/2.a31e8f1d.chunk.js"
+  },
+  {
+    "revision": "ae8468e925f6e53a0a81",
+    "url": "/static/js/main.075bff0b.chunk.js"
+  },
+  {
+    "revision": "b449bc4e6a1ee18b7675",
+    "url": "/static/js/runtime-main.d0908954.js"
+  }
+]);
